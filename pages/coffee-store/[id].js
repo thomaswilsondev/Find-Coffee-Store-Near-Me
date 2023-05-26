8cba45918a6cfe008e100b627c2604f59bc399e2
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-05-27 01:13:55 +0700

    Fallback in Next.js

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -4,10 +4,11 @@ import coffeeStoresData from "../../data/coffee-stores.json";
 
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  console.log("params", params);
   return {
     props: {
       coffeeStore: coffeeStoresData.find((coffeeStore) => {
-        return coffeeStore.id === 0; //dynamic id
+        return coffeeStore.id.toString() === params.id; //dynamic id
       }),
     },
   };
@@ -15,12 +16,14 @@ export async function getStaticProps(staticProps) {
 export function getStaticPaths() {
   return {
     paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
   };
 }
 
-const CoffeeStore = () => {
+const CoffeeStore = (props) => {
   const router = useRouter();
   console.log("router", router);
+  console.log("props", props);
   return (
     <div>
       Coffee Store Page {router.query.id}
@@ -30,6 +33,8 @@ const CoffeeStore = () => {
       <Link href="/coffee-store/dynamic">
         <a>Go to page dynamic</a>
       </Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
   );
 };
