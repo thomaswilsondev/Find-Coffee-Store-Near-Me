f9f7d6b7f5ad3c9e6e8b684dc6570622e3585124
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-05-27 00:45:24 +0700

    Card Component Styling: card glassmorphism

--- components/card/card.js
+++ components/card/card.js
@@ -1,11 +1,26 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import cls from "classnames";
+
+import styles from "./card.module.css";
+
 const Card = (props) => {
   return (
-    <Link href={props.imgUrl}>
-      <a>
-        <h2>{props.name}</h2>
-        <Image src={props.imgUrl} width={260} height={160} />
+    <Link href={props.href}>
+      <a className={styles.cardLink}>
+        <div className={cls("glass", styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={props.imgUrl}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   );
