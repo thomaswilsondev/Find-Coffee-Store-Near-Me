285e8312a14781e712268edb092fbdd2b002c193
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-05-26 23:50:02 +0700

    addtion router

---
+++ pages/[dynamic].js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const DynamicRoute = () => {
+  const router = useRouter();
+  const query = router.query.dynamic;
+  console.log("query", query);
+
+  return (
+    <>
+      <Head>
+        <title>{query}</title>
+      </Head>
+      <div>{` Hi There I am a dynamic route: ${query}`}</div>;
+    </>
+  );
+};
+
+export default DynamicRoute;
---
+++ pages/courses/nextjs.js
@@ -0,0 +1,4 @@
+const NextJs = () => {
+  return <div>Welcome to Next.js with Ankita</div>;
+};
+export default NextJs;
