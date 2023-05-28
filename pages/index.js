e94373e3d8ea6f8d87a0ae67ae8beb89dad2562b
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-05-28 18:15:07 +0700

    Refactor Coffee Store App

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useContext } from "react";
-import { ACTION_TYPES, StoreContext } from "./_app";
+import { ACTION_TYPES, StoreContext } from "../store/store-context";
 import Banner from "../components/banner/banner";
 import Card from "../components/card/card";
 
