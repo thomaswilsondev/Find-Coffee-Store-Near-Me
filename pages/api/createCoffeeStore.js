e5114832cd857acba77b7f75d555ecc17c7ca9d6
Lê Tấn Bảo Bảo <110554959+thomaswilsondev@users.noreply.github.com>
2023-06-01 11:04:34 +0700

    add create coffee-store and api error handling

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,6 +1,73 @@
 const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_KEY
+const base = new Airtable({ apiKey: "key4DkHhRaUI7Qtx4" }).base(
+  "appJ8VyKa8asnMKMq"
 );
+const table = base("coffee-store");
 
-const table = base("coffee-stores");
+console.log({ table });
+const createCoffeeStore = async (req, res) => {
+  console.log({ req });
+
+  //CREATE A RECORDS
+  if (req.method === "POST") {
+    const { id, name, neighbourhood, address, imgUrl, voting } = req.body[0];
+    try {
+      //CHECK ID
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
+
+        console.log({ findCoffeeStoreRecords });
+
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = findCoffeeStoreRecords.map((record) => {
+            return {
+              ...record.fields,
+            };
+          });
+          res.json(records);
+        } else {
+          //create a record
+          //CHECK NAME
+          if (name) {
+            const createRecords = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  voting,
+                  imgUrl,
+                },
+              },
+            ]);
+
+            const records = createRecords.map((record) => {
+              return {
+                ...record.fields,
+              };
+            });
+            res.json(records);
+          } else {
+            //ERROR ID AND NAME
+            res.status(400).json({ message: "Id or name is missing" });
+          }
+        }
+      } else {
+        //ERROR ID
+        res.status(400).json({ message: "Id is missing" });
+      }
+    } catch (err) {
+      ///ERROR CREATE OR  FINDING A STORE
+      console.error("Error creating or finding a store", err);
+      res
+        .status(500)
+        .json({ message: "Error creating or finding a store", err });
+    }
+  }
+};
+export default createCoffeeStore;
--- pages/api/getCoffeeStoresByLocation.js
+++ pages/api/getCoffeeStoresByLocation.js
@@ -6,12 +6,10 @@ const getCoffeeStoresByLocation = async (req, res) => {
   try {
     const { latLong, limit } = req.query;
     const response = await fetchCoffeeStores(latLong, limit);
-    res.status(200);
-    res.json(response);
+    res.status(200).json(response);
   } catch (err) {
     console.error("There is an error", err);
-    res.status(500);
-    res.json({ message: "Oh no! Something went wrong", err });
+    res.status(500).json({ message: "Oh no! Something went wrong", err });
   }
 
   //return
